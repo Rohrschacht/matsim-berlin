0943082e13743621f7c28f50a4846e61425dbe7f
stendler <git@stendler.de>
2022-06-27 17:00:43 +0200

    analyze affected agents: refactor to simplify

--- src/main/java/org/matsim/analysis/AffectedAgents.java
+++ src/main/java/org/matsim/analysis/AffectedAgents.java
@@ -3,6 +3,7 @@ package org.matsim.analysis;
 import org.locationtech.jts.geom.Geometry;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.*;
+import org.matsim.core.router.TripStructureUtils;
 import org.matsim.prepare.homework1.CoordinateGeometryUtils;
 
 import java.util.HashSet;
@@ -14,31 +15,26 @@ public class AffectedAgents {
 	 * Return all persons, which have a plan within the given geometry.
 	 */
 	public static Set<Person> fromGeometry(Population population, Network network, Geometry geometry, String... transportModes) {
-		var coordinateUtils = new CoordinateGeometryUtils(CoordinateGeometryUtils.TRANSFORMATION_UMWELTZONE);
-		var links = network.getLinks();
+		var coordinateUtils = new CoordinateGeometryUtils(CoordinateGeometryUtils.TRANSFORMATION_UMWELTZONE, network);
 
 		var affectedPersons = new HashSet<Person>();
 
 		persons:
 		for (Person person : population.getPersons().values()) {
 			for (Plan plan : person.getPlans()) {
-				for (PlanElement elem : plan.getPlanElements()) {
-					if (elem instanceof Leg) {
-						Leg leg = (Leg) elem;
-						if (coordinateUtils.isCoordInGeometry(links.get(leg.getRoute().getStartLinkId()).getCoord(), geometry) || coordinateUtils.isCoordInGeometry(links.get(leg.getRoute().getEndLinkId()).getCoord(), geometry)) {
-							if (transportModes.length == 0) { // default all
-								affectedPersons.add(person);
-								continue persons; // this person is affected, continue with next
-							} else {
-								for (String mode : transportModes) {
-									if (leg.getMode().equals(mode)) {
-										affectedPersons.add(person);
-										continue persons; // this person is affected, continue with next
-									}
+				for (Leg leg : TripStructureUtils.getLegs(plan.getPlanElements())) {
+					if (coordinateUtils.isLegInGeometry(leg, geometry)) {
+						if (transportModes.length == 0) { // default all
+							affectedPersons.add(person);
+							continue persons; // this person is affected, continue with next
+						} else {
+							for (String mode : transportModes) {
+								if (leg.getMode().equals(mode)) {
+									affectedPersons.add(person);
+									continue persons; // this person is affected, continue with next
 								}
 							}
 						}
-
 					}
 				}
 			}
@@ -51,31 +47,26 @@ public class AffectedAgents {
 	 * Return all persons, which have a plan within the given geometry. And vehicle types.
 	 */
 	public static Set<Person> fromGeometrySelectedPlansOnly(Population population, Network network, Geometry geometry, String... transportModes) {
-		var coordinateUtils = new CoordinateGeometryUtils(CoordinateGeometryUtils.TRANSFORMATION_UMWELTZONE);
-		var links = network.getLinks();
+		var coordinateUtils = new CoordinateGeometryUtils(CoordinateGeometryUtils.TRANSFORMATION_UMWELTZONE, network);
 
 		var affectedPersons = new HashSet<Person>();
 
 		persons:
 		for (Person person : population.getPersons().values()) {
-				for (PlanElement elem : person.getSelectedPlan().getPlanElements()) {
-					if (elem instanceof Leg) {
-						Leg leg = (Leg) elem;
-						if (coordinateUtils.isCoordInGeometry(links.get(leg.getRoute().getStartLinkId()).getCoord(), geometry) || coordinateUtils.isCoordInGeometry(links.get(leg.getRoute().getEndLinkId()).getCoord(), geometry)) {
-							if (transportModes.length == 0) { // default all
+			for (Leg leg : TripStructureUtils.getLegs(person.getSelectedPlan().getPlanElements())) {
+				if (coordinateUtils.isLegInGeometry(leg, geometry)) {
+					if (transportModes.length == 0) { // default all
+						affectedPersons.add(person);
+						continue persons; // this person is affected, continue with next
+					} else {
+						for (String mode : transportModes) {
+							if (leg.getMode().equals(mode)) {
 								affectedPersons.add(person);
 								continue persons; // this person is affected, continue with next
-							} else {
-								for (String mode : transportModes) {
-									if (leg.getMode().equals(mode)) {
-										affectedPersons.add(person);
-										continue persons; // this person is affected, continue with next
-									}
-								}
 							}
 						}
-
 					}
+				}
 			}
 		}
 
